dbb29415f83eedea8e61d5b1abe2632c9e79a940
Tomasz Staśkiewicz <stwt@viessmann.com>
2021-09-07 13:10:59 +0200

    ert

--- src/nope/Maine.groovy
+++ src/nope/Maine.groovy
@@ -7,8 +7,6 @@ def buildImage(args = [:]) {
     def credentials = [azureServicePrincipal(credentialsId: credentialsId, clientIdVariable: 'clientId', clientSecretVariable: 'clientSecret')]
 
         withCredentials(credentials) {
-            sh "apt update && apt install curl -y"
-            sh "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
             sh "az aks get-credentials --admin --name ev4aks --resource-group ev4-aks-rg -f - >  ${WORKSPACE}/.kubeconfig"
             sh "KUBECONFIG=${WORKSPACE}/.kubeconfig"
             sh "kubectl config get-contexts"
