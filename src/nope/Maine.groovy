a3d5e25ee856f2b8e59019683b0c786aedd827f0
Tomasz Staśkiewicz <stwt@viessmann.com>
2021-09-07 13:30:01 +0200

    test credentials

--- src/nope/Maine.groovy
+++ src/nope/Maine.groovy
@@ -7,9 +7,11 @@ def buildImage(args = [:]) {
     def credentials = [azureServicePrincipal(credentialsId: credentialsId, clientIdVariable: 'clientId', clientSecretVariable: 'clientSecret')]
 
         withCredentials(credentials) {
-            sh "az aks get-credentials --admin --name ev4aks --resource-group ev4-aks-rg -f - > /home/.kubeconfig"
-            sh "KUBECONFIG=/home/.kubeconfig"
-            sh "kubectl config get-contexts"
+            sh "az aks get-credentials --admin --name ev4aks --resource-group ev4-aks-rg " // -f - > /home/.kubeconfig"
+            // sh "KUBECONFIG=/home/.kubeconfig"
+            // sh "kubectl config get-contexts"
+            sh "az account show --query tenantId -o tsv"
+            sh "az account show --query id -o tsv"
             }
 }
 
