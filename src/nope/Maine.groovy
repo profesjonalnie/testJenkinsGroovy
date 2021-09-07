b5635cfceea709a633a85fcd44f87f567136fc85
Tomasz Staśkiewicz <stwt@viessmann.com>
2021-09-07 11:47:34 +0200

    fix

--- src/nope/Maine.groovy
+++ src/nope/Maine.groovy
@@ -3,6 +3,15 @@ package nope
 def buildImage(args = [:]) {
 
     print "abcfwefefawfwrawfawfawfafafa aaaaaaaaa"
+    def credentialsId = args.credentialsId
+    def credentials = [azureServicePrincipal(credentialsId: credentialsId, clientIdVariable: 'clientId', clientSecretVariable: 'clientSecret')]
+
+        withCredentials(credentials) {
+            sh "az aks get-credentials --admin --name ${clusterName}aks --resource-group ${clusterName}-aks-rg -f - >  ~/.kube/config"
+            sh "az account show --query id -o tsv"
+            }
 }
+
+
 
 return this
