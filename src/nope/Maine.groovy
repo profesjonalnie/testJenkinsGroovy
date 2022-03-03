92dea0bbd4bfaf240264db78a24276bdb3a0ed7d
Tomasz Staśkiewicz <stwt@viessmann.com>
2022-03-03 09:27:38 +0100

    asdasd

--- src/nope/Maine.groovy
+++ src/nope/Maine.groovy
@@ -51,8 +51,7 @@ def testingOne(args = [:]){
         print args.TAG_NAME
     }
     imported.greet()
-    build("fisrt")
-
+    build(job: "fisrt/master/124")
     // assert  :"You have to provide projectName"
 
     if(args.abc){
